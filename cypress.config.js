cb40c225afb9b82c509533ab7c27457ed025308f
AndreyCar <kravchenko.svg@gmail.com>
2022-07-17 23:14:57 +0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress');
+const AllureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-	e2e: {
-		setupNodeEvents(on, config) {
-			AllureWriter(on, config);
-			return config;
-		},
-	},
+  e2e: {
+      setupNodeEvents(on, config) {
+          AllureWriter(on, config);
+          return config;
+      }
+  }
 });
